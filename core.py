55d682a70f932bd6b51e2040e8d6b51a3f7e744a
Strideface <77002479+Strideface@users.noreply.github.com>
2025-05-15 18:13:11 +0100

    WIP
    testing daily stats

--- core.py
+++ core.py
@@ -1,14 +1,8 @@
 import csv
 import openpyxl
 
-def read_xlsx(file_name):
-    # with open(file_name,'r',encoding="Latin-1",newline='') as f:
-    #     #encoding with 'utf-8' causes 'UnicodeDecodeError, invalid continuation byte' error. 
-    #     #'For NBA__WNBA_Daily_Stats.xlsx', 'latin-1' is used to encode.
-    #     reader = csv.DictReader(f)
-    #     # print(reader)
-    #     for row in reader:
-    #         print(row)
+
+def read_NBA_WNBA(file_name):
 
     wb = openpyxl.load_workbook(file_name) 
     #get names of all tabs in the spreadsheet
@@ -18,15 +12,32 @@ def read_xlsx(file_name):
     for tab_name in tab_names_list:
         worksheets.append(wb.get_sheet_by_name(tab_name))
 
-    #return worksheet objects which then feeds into filter_stats_from_worksheets
+    #return worksheet objects which then feeds into filter_NBA_WNBA
     return {'NBA': worksheets[0], 'WNBA': worksheets[1]}
 
-def filter_stats_from_worksheets(worksheets_dict):
+
+def read_daily_stats(file_name):
+
+    wb = openpyxl.load_workbook(file_name) 
+    #get names of all tabs in the spreadsheet
+    tab_names_list = wb.sheetnames
+    #return the worksheet objects for each of those names
+    worksheets = []
+    for tab_name in tab_names_list:
+        worksheets.append(wb.get_sheet_by_name(tab_name))
+
+    #return worksheet objects which then feeds into filter_daily_stats
+    return {'Current Backlog': worksheets[0], 'Daily Support Contacts': worksheets[1], 
+            'Previous Day P1P2 Ticket...': worksheets[2]} 
+
+
+def filter_NBA_WNBA(worksheets_dict):
     
     NBA_emails = 0
     NBA_chats = 0
     WNBA_emails = 0
 
+    #iterate through the items in the rows to add up values of emails and seperate them from chats
     for row in worksheets_dict['NBA'].iter_rows(values_only=True):
         for item in row:
             if 'Email' == item:
@@ -38,10 +49,7 @@ def filter_stats_from_worksheets(worksheets_dict):
             elif 'Messaging' == item:
                 NBA_chats += int(float(row[2]))
 
-    print(str(NBA_emails) + ', ' + str(NBA_chats))
-
-    
-    print('WNBA:')
+    #same as above but only need the total figure ('SUM')
     for row in worksheets_dict['WNBA'].iter_rows(values_only=True):
         for item in row:
             if 'SUM' == item:
@@ -49,9 +57,23 @@ def filter_stats_from_worksheets(worksheets_dict):
                 break
                 # break as there are two 'SUM' items in the row (don't want to add twice)
 
-    print(str(WNBA_emails))
-
+    #feeds into the creation of a new csv file function
     return {'NBA - email': NBA_emails, 'NBA - chat': NBA_chats, 'WNBA - email': WNBA_emails}
+
+
+def filter_daily_stats(worksheets_dict):
+
+    for row in worksheets_dict['Current Backlog'].iter_rows(values_only=True):
+        print(row)
+
+    for row in worksheets_dict['Daily Support Contacts'].iter_rows(values_only=True):
+        print(row)
+
+    for row in worksheets_dict['Previous Day P1P2 Ticket...'].iter_rows(values_only=True):
+        print(row)
+
+    return
+
 
 def create_csv(rows_dict):
     print(rows_dict)
--- run_script.py
+++ run_script.py
@@ -1,9 +1,13 @@
 import core
 
-worksheets_dict = core.read_xlsx('NBA__WNBA_Daily_Stats.xlsx')
+# worksheets_dict = core.read_NBA_WNBA('NBA__WNBA_Daily_Stats.xlsx')
 
-rows_dict = core.filter_stats_from_worksheets(worksheets_dict)
+# rows_dict = core.filter_NBA_WNBA(worksheets_dict)
 
-core.create_csv(rows_dict)
+# core.create_csv(rows_dict)
+
+worksheets_dict = core.read_daily_stats('Daily_Stats_L1_Support.xlsx')
+
+core.filter_daily_stats(worksheets_dict)
 
 print("Finished")
